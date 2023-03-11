ab715143b94bc1adb96d6e65ff7f534e42304105
prathamesh <prathameshamundkar@gmail.com>
2023-03-11 19:57:06 +0530

    learning this best space complexity algo

--- 02_arrays/08_remove_efficient_solution.cpp
+++ 02_arrays/08_remove_efficient_solution.cpp
@@ -4,27 +4,16 @@ using namespace std;
 
  int remDups(int arr[], int n)
 {
-	int temp[n];
+	// tc=o(n) sc=o(1)
 
-	temp[0] = arr[0];
-
-	int res = 1;
-
-	for(int i = 1; i < n; i++)
-	{
-		if(temp[res - 1] != arr[i])
-		{
-			temp[res] = arr[i];
-			res++;
-		}
-	}
-
-	for(int i = 0; i < res; i++)
-	{
-		arr[i] = temp[i];
-	}
-
-	// n=10;
+    int res=1;
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[res-1]){
+            arr[res]=arr[i];
+            res++;
+        }
+    }
+    return res;
 }
     
 int main() {
@@ -40,11 +29,12 @@ int main() {
 
        cout<<endl;
 
-       remDups(arr, n);
+      int np= remDups(arr, n);
+      cout<<"the new size is "<<np<<endl;
 
        cout<<"After Removal"<<endl;
 
-       for(int i = 0; i < n; i++)
+       for(int i = 0; i < np; i++)
        {
        		cout<<arr[i]<<" ";
        }
